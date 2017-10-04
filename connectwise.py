478ff9571a7d9b0d2c34d0d92e7c322ed28b5eee
Joshua M Smith <saether@gmail.com>
2017-10-03 17:15:29 -0700

    Update import of ConnectPyse library

--- connectwise.py
+++ connectwise.py
@@ -8,8 +8,8 @@ Tasks:
 
 import sys
 
-import connectpyse
-from connectpyse.service import tickets_api
+from ConnectPyse.service import tickets_api
+from ConnectPyse.company import companies_api, contacts_api
 
 
 def lookup_sr(ticket_num):
@@ -27,11 +27,41 @@ def lookup_sr(ticket_num):
     return ''
 
 
+def lookup_company(company_id):
+    if company_id.isdigit():
+        try:
+            company = companies_api.CompaniesAPI().get_company_by_id(company_id)
+        except connectpyse.restapi.ApiError as err:
+            ticket = 'error'
+            print("API error:", err)
+            raise
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+        return company
+    return ''
+
+
+def lookup_contact(contact_id):
+    if contact_id.isdigit():
+        try:
+            contact = contacts_api.ContactsAPI().get_contact_by_id(contact_id)
+        except connectpyse.restapi.ApiError as err:
+            ticket = 'error'
+            print("API error:", err)
+            raise
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+        return contact
+    return ''
+
+
 def return_sr_summary(ticket_id):
     ticket = lookup_sr(ticket_id)
     if ticket.id:
-        # print(ticket)
         return '[Ticket #{}](https://na.myconnectwise.net/v4_6_release/services/system_io/Service/' \
-               'fv_sr100_request.rails?service_recid={}&companyName={}): {} '.format(ticket.id, ticket.id, 'wheelhouseit', ticket.summary, )
+               'fv_sr100_request.rails?service_recid={}&companyName={}): {}\n{}/{} '.\
+            format(ticket.id, ticket.id, 'wheelhouseit', ticket.summary, ticket.company['name'], ticket.contact['name'])
     else:
         return 'Error: {}'.format(ticket_id)
